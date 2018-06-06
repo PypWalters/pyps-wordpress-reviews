cf095835d7490c6fa6ef03e6efb78c2b126f07d9
Stephanie <spypniowski@gmail.com>
2018-06-06 09:33:47 -0400

    cleaned up code formatting

--- pyp-simple-reviews.php
+++ pyp-simple-reviews.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Pyp Simple WordPress Reviews
-Description: Adds the option for 5 star reviews on native wordpress comments
+Description: Adds the option for 5 star reviews on native WordPress comments
 Version: 1.1
 Author: Stephanie Walters
 Author URI: https://pypwalters.com
@@ -25,24 +25,24 @@ define( 'PLUGIN_NAME_VERSION', '1.1.0' );
  * Includes the admin menu settings
  */
 if ( file_exists( __DIR__ . '/includes/pyp-reviews-options.php' ) ) {
-    include_once __DIR__ . '/includes/pyp-reviews-options.php';
-    add_action( 'plugins_loaded', array( 'Pyp_Reviews_Options', 'get_instance' ), 15 );
+	include_once __DIR__ . '/includes/pyp-reviews-options.php';
+	add_action( 'plugins_loaded', array( 'Pyp_Reviews_Options', 'get_instance' ), 15 );
 }
 
 /**
  * Includes the user interface to add a rating to a comment
  */
 if ( file_exists( __DIR__ . '/includes/pyp-reviews-user-input.php' ) ) {
-    include_once __DIR__ . '/includes/pyp-reviews-user-input.php';
-    add_action( 'plugins_loaded', array( 'Pyp_Reviews_Create_Rating', 'get_instance' ), 15 );
+	include_once __DIR__ . '/includes/pyp-reviews-user-input.php';
+	add_action( 'plugins_loaded', array( 'Pyp_Reviews_Create_Rating', 'get_instance' ), 15 );
 }
 
 /**
  * Includes the display of saved ratings on comment output
  */
 if ( file_exists( __DIR__ . '/includes/pyp-reviews-comment-output.php' ) ) {
-    include_once __DIR__ . '/includes/pyp-reviews-comment-output.php';
-    add_action( 'plugins_loaded', array( 'Pyp_Reviews_Output', 'get_instance' ), 15 );
+	include_once __DIR__ . '/includes/pyp-reviews-comment-output.php';
+	add_action( 'plugins_loaded', array( 'Pyp_Reviews_Output', 'get_instance' ), 15 );
 }
 
 
@@ -50,9 +50,7 @@ if ( file_exists( __DIR__ . '/includes/pyp-reviews-comment-output.php' ) ) {
  * Enqueue Front-end styles
  */
 function pyp_reviews_styles() {
-    wp_enqueue_style( 'dashicons' );
-		wp_enqueue_style( 'pyp_reviews_styles', plugins_url( 'css/pyp-reviews-styles.css', __FILE__ ), 'dashicons' );
+	wp_enqueue_style( 'dashicons' );
+	wp_enqueue_style( 'pyp_reviews_styles', plugins_url( 'css/pyp-reviews-styles.css', __FILE__ ), 'dashicons' );
 }
 add_action( 'wp_enqueue_scripts', 'pyp_reviews_styles' );
-
-?>
