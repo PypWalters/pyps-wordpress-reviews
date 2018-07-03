df7a4e0d38d9c50d2dae25e8690f54ed59b27807
Stephanie <spypniowski@gmail.com>
2018-07-03 10:27:36 -0400

    added 3 working tests for comment output

--- tests/includes/test-comment-output.php
+++ tests/includes/test-comment-output.php
@@ -3,26 +3,54 @@
  * Pyp_Reviews_Output_Test
  * @group frontend
  */
-use Pyp_Reviews_Output;
+//use Pyp_Reviews_Output;
 
 class Pyp_Reviews_Output_Test extends WP_UnitTestCase {
 
-	/** @test */
-	public function test_display_saved_review() {
-
-		$comment_object = $this->factory->comment->create(
-			array(
-				'meta_input' => array(
-					'pyp_rating' => 4,
-				),
-			)
-		);
-
+	public function test_display_saved_review_w_no_review_meta() {
 		$output_class = new Pyp_Reviews_Output;
+		$expected     = 'This is a test.';
+
+		$comment_args   = array(
+			'comment_content' => 'This is a test.',
+		);
+		$comment_object = $this->factory->comment->create_and_get( $comment_args );
 
 		$this->assertEquals(
-			'Rated 4/5<br />This is a test review.',
-			$output_class->display_saved_review( $comment_object['post_content'], $comment_object )
+			$expected,
+			$output_class->display_saved_review( $comment_object->comment_content, $comment_object )
+		);
+	}
+
+	public function test_display_saved_review_w_good_review_meta() {
+		$comment_args   = array(
+			'comment_content' => 'This is a test.',
+		);
+		$comment_object = $this->factory->comment->create_and_get( $comment_args );
+		add_comment_meta( $comment_object->comment_ID, 'pyp_rating', 4 );
+
+		$output_class = new Pyp_Reviews_Output;
+		$expected     = 'Rated 4/5<br />This is a test.';
+
+		$this->assertEquals(
+			$expected,
+			$output_class->display_saved_review( $comment_object->comment_content, $comment_object )
+		);
+	}
+
+	public function test_display_saved_review_w_nan_review_meta() {
+		$comment_args   = array(
+			'comment_content' => 'This is a test.',
+		);
+		$comment_object = $this->factory->comment->create_and_get( $comment_args );
+		update_comment_meta( $comment_object->comment_ID, 'pyp_rating', 'bad!;\'' );
+
+		$output_class = new Pyp_Reviews_Output;
+		$expected     = 'This is a test.';
+
+		$this->assertEquals(
+			$expected,
+			$output_class->display_saved_review( $comment_object->comment_content, $comment_object )
 		);
 	}
 }
